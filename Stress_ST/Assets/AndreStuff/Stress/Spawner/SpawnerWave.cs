c8008114f74e07b914db6b692e85e1a3747ca1b9
DESKTOP-4R8FPJR\andre <andre-lisle@hotmail.com>
2017-02-07 17:52:50 +0100

    Made it possible to have miniwaves within a wave. changed the spawning method to

--- Stress_ST/Assets/AndreStuff/Stress/Spawner/SpawnerWave.cs
+++ Stress_ST/Assets/AndreStuff/Stress/Spawner/SpawnerWave.cs
@@ -4,6 +4,6 @@ using System.Collections;
 [System.Serializable]
 public class SpawnerWave : MonoBehaviour {
 
-	public CreatureInfoSpawning[] Wave;
+	public MiniWaves[] MiniWaves;
 
 }
--- Stress_ST/Assets/AndreStuff/Stress/Spawner/Spawnmanaging.cs
+++ Stress_ST/Assets/AndreStuff/Stress/Spawner/Spawnmanaging.cs
@@ -10,19 +10,21 @@ public class Spawnmanaging : MonoBehaviour {
 	public bool NextWave = false;
 	public bool SpawnAllAtOnce = false;
 
-	int WaveNumber = 0;
-	int wavecounter = 0;
-	int wavesize = 0;
+	int WaveNumber = 0;	
+	int whichWave = 0;
+	int whichSmallWave = 0;
 	int spawnspot = 0;
 
-	CreatureInfoSpawning[] tes;
 	public SpawnerWave[] TheWaves;
+	MiniWaves[] TheSmallWaves;
+	CreatureInfoSpawning[] ObjectsToSpawn;
 
 	// Use this for initialization
 	void Start () {
 		WaveNumber = TheWaves.Length;
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
@@ -30,28 +32,55 @@ public class Spawnmanaging : MonoBehaviour {
 		
 			if (SpawnAllAtOnce == true || NextWave == true) {
 				NextWave = false;
-				if (wavecounter < WaveNumber) {
-					spawnspot = 0;
-					tes = TheWaves [wavecounter].Wave;
-					wavesize = TheWaves [wavecounter].Wave.Length;
+			
 
-					for (int j = 0; j < wavesize; j++) {
+				if (whichWave < WaveNumber) {
+					TheSmallWaves = TheWaves [whichWave].MiniWaves;//getting the current wave
 
-						for (int k = 0; k < TheWaves [wavecounter].Wave [j].SpawnAmount; k++) {
+					if (whichSmallWave < TheSmallWaves.Length) {
+						ObjectsToSpawn = TheSmallWaves [whichSmallWave].Wave;//getting the current miniwave
 
+						for (int k = 0; k < ObjectsToSpawn.Length; k++) {//spawning the miniwave
+							for (int l = 0; l < ObjectsToSpawn [k].SpawnAmount; l++) {
+								if (spawnspot >= SpawnPoints.Length) {
+									spawnspot = 0;
+								}
 
-							if (spawnspot >= SpawnPoints.Length) {
-								spawnspot = 0;
+								Instantiate (ObjectsToSpawn [k].creature.gameObject, SpawnPoints [spawnspot].transform.position, Quaternion.identity, SpawnPoints [spawnspot++].transform);
 							}
-
-							Instantiate (tes [j].creature.gameObject, SpawnPoints [spawnspot].transform.position, Quaternion.identity, SpawnPoints [spawnspot++].transform);
 						}
-					}
-		
-					wavecounter++;
-
+						whichSmallWave++;
+						if (whichSmallWave == TheSmallWaves.Length) {//if the miniwave amount is bigge then the length of the list, go to the next wave and reset the miniwavecounter
+							whichSmallWave = 0;
+							whichWave++;
+						}
+					} 
 				}
 			}
 		}
 	}
 }
+/*
+ 		for (int i = 0; i < WaveNumber; i++) {
+					TheSmappWaves = TheWaves [i].MiniWaves;//henter wave 1.
+					for (int j = 0; j < TheSmappWaves.Length; j++) {
+						tes = TheSmappWaves [j].Wave;//finner miniwave 1.
+					
+
+
+						for (int k = 0; k < tes.Length; k++) {
+							for (int l = 0; l < tes [k].SpawnAmount; l++) {
+								if (spawnspot >= SpawnPoints.Length) {
+									spawnspot = 0;
+								}
+
+								Instantiate (tes [k].creature.gameObject, SpawnPoints [spawnspot].transform.position, Quaternion.identity, SpawnPoints [spawnspot++].transform);
+							}
+						}
+
+
+
+
+					}
+				}
+*/

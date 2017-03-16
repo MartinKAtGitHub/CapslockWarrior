9caec308c2ddd947ec41bc6c8a3e0e1bde3284b1
DESKTOP-4R8FPJR\andre <andre-lisle@hotmail.com>
2017-03-16 16:47:00 +0100

    ehrmn, made the changeing map thing that i talked about some time ago. a simple version, just some walls rising up from the ground.   made it in AndreSpawner scene -> (Assets\AndreStuff\TestingFolder\andreSpawnerTesting\AndreSpawner.unity) if you want to take a look.

---
+++ Stress_ST/Assets/AndreStuff/TestingFolder/WallTest/RunAnimationIfPlayerClose.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunAnimationIfPlayerClose : MonoBehaviour {
+
+	public Transform Player;
+	public Transform me;
+
+	float distance = 5f;
+	// Use this for initialization
+	void Start () {
+		me = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (distance <= Vector3.Distance (Player.position, me.position)) {
+			GetComponent<Animator> ().SetInteger ("Change", 2);
+		} else {
+			GetComponent<Animator> ().SetInteger ("Change", 1);
+		}
+	}
+}

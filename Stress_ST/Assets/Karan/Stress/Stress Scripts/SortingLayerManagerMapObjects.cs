5b50a267732c64701e8304add212eadcfc8f688a
k singh <martinsingh91@gmail.com>
2017-03-23 19:48:25 +0100

    Added sound test, order layer logic

---
+++ Stress_ST/Assets/Karan/Stress/Stress Scripts/SortingLayerManagerMapObjects.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[ExecuteInEditMode]
+public class SortingLayerManagerMapObjects : MonoBehaviour {
+
+	Renderer spriteRendrer;
+	// Use this for initialization
+	void Start () 
+	{
+		spriteRendrer = GetComponent<Renderer>();
+		spriteRendrer.sortingOrder = (int)(transform.position.y * -10);
+	}
+
+
+	// 
+	#if UNITY_EDITOR
+	void Update () 
+	{
+		// We want to do this in Start in the final game since thay are static objects that wont move.
+		spriteRendrer.sortingOrder = (int)(transform.position.y * -10);
+		//Debug.Log("TEST in EDITOR");
+	}
+	#endif
+}

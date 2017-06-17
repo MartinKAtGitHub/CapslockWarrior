c20fa2fe1e1e3d80dc02fe60f9918d4c037d8f77
DESKTOP-4R8FPJR\andre <andre-lisle@hotmail.com>
2017-06-17 01:15:18 +0100

    Added The Complete Sheep character + mole "walk" and attack animation

---
+++ Stress_ST/Assets/AndreStuff/TestingFolder/DiggingMole/xMoleTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xMoleTest : MonoBehaviour {
+
+	public float movevalue = 1;
+	Animator myanim;
+	AnimatorControllerParameter test ;
+	xSheepTest2 animatorscript;
+	public GameObject target;
+	public GameObject spike;
+	// Use this for initialization
+	void Start () {
+		myanim = GetComponent<Animator> ();
+		animatorscript = myanim.GetBehaviour<xSheepTest2> ();
+		animatorscript.Animationfinished = false;
+		movevalue = -1;
+
+		movevect = (transform.position - target.transform.position).normalized;
+		lastdist = startDist;
+	}
+
+	float dist = 0;
+	Vector3 movevect = Vector3.zero;
+	float startDist = 3;
+	float distinbetween = 0.49f;
+	float lastdist = 0;
+
+	void Update (){
+		dist = Vector3.Distance (target.transform.position, transform.position);
+
+		if (dist < lastdist) {
+			lastdist -= distinbetween;
+			Instantiate (spike, transform.position, Quaternion.identity);
+		}
+
+		if (animatorscript.Animationfinished == true) {
+			transform.position += movevect * Time.deltaTime * movevalue;
+		}
+	}
+}

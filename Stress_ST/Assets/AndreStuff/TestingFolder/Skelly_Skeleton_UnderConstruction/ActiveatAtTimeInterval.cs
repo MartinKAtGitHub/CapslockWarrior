98eb23c5081feec3f7eb914a1e8e50e1b101fede
DESKTOP-4R8FPJR\andre <andre-lisle@hotmail.com>
2017-07-15 02:39:42 +0100

    added a new attack animation for the boss + testing fireball style

---
+++ Stress_ST/Assets/AndreStuff/TestingFolder/Skelly_Skeleton_UnderConstruction/ActiveatAtTimeInterval.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveatAtTimeInterval : MonoBehaviour {
+
+	public float Times = 0;
+	public int Children = 0;
+	public float counter = 0;
+	public int child = 0;
+	// Update is called once per frame
+
+	void Update () {
+		counter += Time.deltaTime;
+
+		if (counter > Times) {
+			counter = 0;
+			transform.GetChild (child++).transform.gameObject.SetActive (true);
+			if (child >= Children) {
+				this.enabled = false;
+			}
+		}
+
+	}
+}
---
+++ Stress_ST/Assets/AndreStuff/TestingFolder/Skelly_Skeleton_UnderConstruction/FireWave.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWave : MonoBehaviour {
+
+	Vector3 MyDirection = Vector3.zero;
+	float counter = 0;
+//	Vector3 EulerAngles = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		MyDirection = Quaternion.Euler (0,0,transform.rotation.eulerAngles.z - 45f) * Vector3.right;
+
+	//	EulerAngles.z = transform.eulerAngles.z;
+
+	//	EulerAngles.z = 25 * Time.deltaTime;
+	//	transform.eulerAngles += EulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter += Time.deltaTime;
+
+		if (counter > 7) {
+			counter = 0;
+			transform.localPosition = Vector3.zero;
+		}
+			
+	//	transform.eulerAngles += EulerAngles;
+		transform.position += MyDirection * Time.deltaTime;
+	}
+}

05bd2ef64cf6678f383f56dbca8e3dc3dd64c9b1
k singh <martinsingh91@gmail.com>
2017-07-27 17:25:49 +0200

    Some UI Changes/ added Spell bar. some code

---
+++ Stress_ST/Assets/Karan/Stress/Stress Scripts/Camera Scripts/CameraTESTViewAspectReselutionChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTESTViewAspectReselutionChange : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		Camera.main.aspect = 4f/3f;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

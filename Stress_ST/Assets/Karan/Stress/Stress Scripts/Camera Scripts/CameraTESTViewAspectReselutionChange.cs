c81eb7499834f409828282ef2739b0f29750fedb
k singh <martinsingh91@gmail.com>
2017-11-03 19:42:58 +0100

    Testing and Loadig anim system

--- Stress_ST/Assets/Karan/Stress/Stress Scripts/Camera Scripts/CameraTESTViewAspectReselutionChange.cs
+++ Stress_ST/Assets/Karan/Stress/Stress Scripts/Camera Scripts/CameraTESTViewAspectReselutionChange.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class CameraTESTViewAspectReselutionChange : MonoBehaviour {
 
+public int Width;
+public int Hight;
+// THIS WILL KEEP THE CAM SIZE NO MATTER THE RESULOTION
 
+// THE ASPECT WILL BE PERCERVED WHEN CHANGEING THE RESELUTON.
 
-	// Use this for initialization
+// POINT IS ---------> You WIll always see the same thing not MORE OR LESS depending os RESELUTION 
+
+// MOTE THIS DOSE NOT WORK WITH PIXEL PERFECT!!!!!!!!!!!
+
 	void Start () 
 	{
-		Camera.main.aspect = 4f/3f;	
+		Camera.main.aspect = 1.777778f;	// For some reason when i do Width(16)/Hight(9) i get 1. I should be getting 1.777778f
+
+		Debug.Log("Forcing ASPECT ratio -> " + Camera.main.aspect);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//Debug.Log("SETTING ASPECT " + Camera.main.aspect);
 	}
 }

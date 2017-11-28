6b67d4432019aedb3ae5755e24df15b72d81655f
k singh <martinsingh91@gmail.com>
2017-11-28 21:11:56 +0100

    Moved some scriptes and connected animators

---
+++ Stress_ST/Assets/Karan/Stress/MainMenu_componants/Scripts/MainMenuAnimManager.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuAnimManager : MonoBehaviour {
+
+	
+	public GameObject MainMenuCanvasRoot;
+
+	[Space(5f)]
+	public GameObject UI_ExitPower;
+	public GameObject UI_EndlessModePhone;
+	public GameObject UI_PCMonitor;
+	public GameObject UI_OptionsTable;
+
+	[Space(5f)]
+	public GameObject Anim_MenuCharactor;
+
+	[Header("Names of GameObjects we need in this script")] // Copy and past stopped working Buggy shitt
+	[Space(10)]
+	public string MainMenuCanvasName;
+	public string ExitPowerName = "Exit PowerButton";
+	public string EndlessModePhoneName = "EndlessMode Phone";
+	public string PCMonitorName = "PC Monitor";
+	public string OptionsTableName = "OptionsTable";
+
+	private Animator animatorExitPower;
+	private Animator animatorEndlessModePhone;
+	private Animator animatorPCMonitor;
+	private Animator animatorOptionsTable;
+
+	private Button btn_ExitPower;
+	private Button btn_EndlessModePhone;
+	private Button btn_PCMonitor;
+	private Button btn_OptionsTable;
+
+	private Text txt_ExitPower;
+	private Text txt_EndlessModePhone;
+	private Text txt_PCMonitor;
+	private Text txt_OptionsTable;
+	// Use this for initialization
+	void Awake () 
+	{
+		NullCheckGameObject.NullCheckFindWithTag(ref MainMenuCanvasRoot, "MainCanvas");
+		NullCheckGameObject.NullCheckFindWithName(ref UI_ExitPower , ExitPowerName);
+		NullCheckGameObject.NullCheckFindWithName(ref UI_EndlessModePhone , EndlessModePhoneName);
+		NullCheckGameObject.NullCheckFindWithName(ref UI_PCMonitor , PCMonitorName);
+		NullCheckGameObject.NullCheckFindWithName(ref UI_OptionsTable , OptionsTableName);
+
+		animatorExitPower = UI_ExitPower.GetComponent<Animator>();
+		animatorEndlessModePhone = UI_EndlessModePhone.GetComponent<Animator>();
+		animatorPCMonitor = UI_PCMonitor.GetComponent<Animator>();
+		animatorOptionsTable = UI_OptionsTable.GetComponent<Animator>();
+
+		btn_ExitPower = UI_ExitPower.GetComponentInChildren<Button>();
+		btn_EndlessModePhone = UI_EndlessModePhone.GetComponentInChildren<Button>();
+		btn_PCMonitor = UI_PCMonitor.GetComponentInChildren<Button>();
+		btn_OptionsTable = UI_OptionsTable.GetComponentInChildren<Button>();
+
+		txt_ExitPower = UI_ExitPower.GetComponentInChildren<Text>();
+		txt_EndlessModePhone = UI_EndlessModePhone.GetComponentInChildren<Text>();
+		txt_PCMonitor = UI_PCMonitor.GetComponentInChildren<Text>();
+		txt_OptionsTable = UI_OptionsTable.GetComponentInChildren<Text>();
+	}
+
+
+	public void LoadeStoryModeScene()
+	{
+		// Needs to be playerd after Animation. 
+		Debug.Log("Loade StoryMode scene!");
+	}
+
+	public void OnClickStoryMode()
+	{
+		IsAllButtonsInteractable(false);
+		DisableAllText();
+	}
+
+
+
+	void IsAllButtonsInteractable(bool interactable)
+	{
+		btn_ExitPower.interactable = interactable;
+		btn_EndlessModePhone.interactable = interactable;
+		btn_PCMonitor.interactable = interactable;
+		btn_OptionsTable.interactable = interactable;
+	}
+
+	void DisableAllText()
+	{
+		txt_ExitPower.enabled = false;
+		txt_EndlessModePhone.enabled = false;
+		txt_PCMonitor.enabled = false;
+		txt_OptionsTable.enabled = false;
+	}
+}
---
+++ Stress_ST/Assets/Karan/Stress/Stress Scripts/RandomScripts/NullCheckGameObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NullCheckGameObject {
+
+
+
+	public static void NullCheckFindWithTag(ref GameObject _gameObject, string _findTagName) // Key word Ref. I basicly say dont make a copy of Object passed in, just make direct changes
+	{
+		if(_gameObject == null)
+		{
+			Debug.Log("Did not find GameObject Searching with Tag..." );
+			
+			_gameObject = GameObject.FindWithTag(_findTagName);
+
+			if( _gameObject == null)
+			{
+				Debug.Log("Can't Find Tag" + _findTagName + " Object is NULL");	
+			}
+		}
+	}
+
+	public static void NullCheckFindWithName(ref GameObject _gameObject, string _findName)
+	{
+		if(_gameObject == null)
+		{
+			Debug.Log("Did not find GameObject Searching with Name..." );
+			
+			_gameObject = GameObject.Find(_findName);
+
+			if( _gameObject == null)
+			{
+				Debug.Log("Can't Find Name(" + _findName + ") Object is NULL");
+			}
+		}
+	}
+}

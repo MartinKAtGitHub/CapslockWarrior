4bcb807ea1be7ee974dc3cc00b11a4a5064873d8
DESKTOP-4R8FPJR\andre <andre-lisle@hotmail.com>
2017-11-30 15:48:01 +0100

    Updating changes + adding testingscene_Default which is a stript scene with only necessary objects testingscene_V0 is the scene that can be changed, default is just so that we have the unchanged scene stored for just in case something happends

---
+++ Stress_ST/Assets/AndreStuff/TestingFolder/DebugSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugSpawner : MonoBehaviour {
+
+	public Transform SpawnPosition;
+	public List<GameObject> CreaturesToSpawn;
+	public int SpawnCreatureIndex = 0;
+	public bool SpawnNow = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (SpawnNow == true) {
+			SpawnNow = false;
+			Instantiate (CreaturesToSpawn [SpawnCreatureIndex], SpawnPosition.position, Quaternion.identity);
+		}
+	}
+}

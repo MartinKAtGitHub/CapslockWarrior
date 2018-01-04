6ce9025081a8147e68b1be18d1112755be77b379
k singh <martinsingh91@gmail.com>
2018-01-04 15:27:32 +0100

    Large amout of small changes. hope this dosent break anything

---
+++ Stress_ST/Assets/Karan/Stress/Stress Scripts/Gamanager scripts/GameManager_ToggleMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_ToggleMenu : MonoBehaviour
+{
+	private GameManager_Master gameManagerMaster;
+	public GameObject menu;
+
+	void OnEnable()
+	{
+		SetInitialRefs();
+		gameManagerMaster.GameOverEvent += ToggleMenu;
+	}
+
+	void OnDisable()
+	{
+		gameManagerMaster.GameOverEvent -= ToggleMenu;
+	}
+
+	void Start()
+	{
+		//ToggleMenu();
+	}
+
+	void Update()
+	{
+		CheckForMenuToggleRequest();
+
+	}
+
+
+	void ToggleMenu()
+	{
+
+		if(menu != null)
+		{
+			menu.SetActive(!menu.activeSelf);
+			gameManagerMaster.IsMenuOn = !gameManagerMaster.IsMenuOn;
+			gameManagerMaster.CallEventMenuToggle();
+		}
+		else
+		{
+			Debug.Log("Asigne UI GM to Sctipt");	
+		}
+	}
+	/// <summary>
+	/// Checks to make sure that you can press ESC and open menu / pause the game. you dont want to be able to pause the game if the menu is already open or in game over
+	/// ----ADD MORE CHECKS HERE IF NEEDED---
+	/// </summary>
+	void CheckForMenuToggleRequest()
+	{
+		if(Input.GetKeyUp(KeyCode.Escape) && !gameManagerMaster.IsGameOver)
+		{
+			ToggleMenu();
+		}
+
+	}
+
+	void SetInitialRefs()
+	{
+		gameManagerMaster = GetComponent<GameManager_Master>();
+	}
+}

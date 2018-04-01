68ead239567735b34381a4527e482048bb4d86b1
k singh <martinsingh91@gmail.com>
2018-04-01 18:45:00 +0200

    Added Timeline Cutscene. Still cant pause

---
+++ Stress_ST/Assets/Karan/Stress/TestFolder/Cutscene_intro_All/CustomClipPlayableTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class CustomClipPlayableTest : PlayableTrack 
+{
+	void OnEnable()
+	{
+		Debug.Log("My custom Timeline Clip ENABLE");
+		CreateClip<PlayableTrack>();
+	}
+
+	void Awake()
+	{
+		Debug.Log("My custom Timeline Clip AWAKE");
+	}
+
+
+}

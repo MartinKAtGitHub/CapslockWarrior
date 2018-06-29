cde7c9a33224b65fdd657175b10199b0f6135993
k singh <martinsingh91@gmail.com>
2018-06-29 22:30:07 +0200

    Added ."vs/" To GitIgnor hopefully it willnot register random vs changes

---
+++ Stress_ST/Assets/Karan/Stress/Scripts/Dialogue Scripts/GroupDialogue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupDialogue : MonoBehaviour {
+
+
+    public NPCDialog[] NPCGroup;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+
+    [System.Serializable]
+    public struct NPCDialog
+    {
+        public string[] Dialogue;
+    }
+}

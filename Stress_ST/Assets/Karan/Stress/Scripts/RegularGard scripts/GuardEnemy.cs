ac60e189715101425abc1713e537fb1374f90335
k singh <martinsingh91@gmail.com>
2018-11-13 19:54:14 +0100

    Added Regual NPC, need to create attack logic.

---
+++ Stress_ST/Assets/Karan/Stress/Scripts/RegularGard scripts/GuardEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardEnemy : MonoBehaviour {
+
+    // stats
+
+    [SerializeField] private Animator guardAnimator;
+
+    private int attackHash = Animator.StringToHash("Attack"); // this seemses so bad this HardCoded trigger
+
+	
+    
+    // Use this for initialization
+	void Start ()
+    {
+		// Connect with all my scripts and out going scripts
+	}
+	
+	
+	void Update ()
+    {
+		
+
+
+	}
+
+    void MeleeAttack()
+    {
+        guardAnimator.SetTrigger(attackHash);
+    }
+
+
+}
